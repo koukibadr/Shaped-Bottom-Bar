920e363c981b7055a61297727ebbd72aa192dc01
Kouki Badr <badreddinekouki1998@gmail.com>
2021-06-27 11:12:37 +0100

    add 3D effect to hexagons shapes

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -53,7 +53,7 @@ class _MyScreenState extends State<MyScreen> {
             this.selectedItem = position;
           });
         },
-        shape: ShapeType.TRIANGLE,
+        shape: ShapeType.ROTATED_HEXAGON,
         shapeColor: Colors.pink,
         with3dEffect: true,
         selectedIconColor: Colors.white,
